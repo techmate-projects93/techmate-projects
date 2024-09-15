1f35a913ef428c44b77758d7ec7e1c6906270ce6
omkar30yadav <omkary357@gmail.com>
2024-09-15 19:59:58 +0530

    changing font

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Link } from 'react-scroll';
-import './Home.css';
-import homeImage from '../assets/img/homeImage.png'; // Update with the actual image file name
+import React from "react";
+import { Link } from "react-scroll";
+import "./Home.css";
+import homeImage from "../assets/img/homeImage.png"; // Update with the actual image file name
 
 function Home() {
   return (
@@ -9,15 +9,30 @@ function Home() {
       <div className="container">
         <div className="row align-items-center">
           <div className="col-lg-6 col-md-12 order-md-2">
-            <img src={homeImage} alt="TechMate" className="img-fluid home-image" />
+            <img
+              src={homeImage}
+              alt="TechMate"
+              className="img-fluid home-image"
+            />
           </div>
           <div className="col-lg-6 col-md-12 text-section order-md-1">
-            <h1 className="display-4">Welcome to <span className="highlight">TechMate</span></h1>
+            <h1 className="display-4">
+              Welcome to <span className="highlight">TechMate</span>
+            </h1>
             <p className="lead">
-              Your partner in web development, UI/UX, landing pages, and social media management.
+              
+              Are you struggling to get your business online? <br></br>We get it—going digital can be tough, but that’s
+              where TechMate comes in. We make it easy with affordable, custom
+              solutions that fit your needs. Whether it’s a website, e-commerce
+              store, or a landing page, we’ve got your back.
             </p>
             <a href="#contact" className="btn btn-light btn-lg">
-              <Link className="nav-link" to="contact" smooth={true} duration={1000}>
+              <Link
+                className="nav-link"
+                to="contact"
+                smooth={true}
+                duration={1000}
+              >
                 Get In Touch
               </Link>
             </a>
